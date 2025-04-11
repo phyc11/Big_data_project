51e16f044babe70440b4acf53fb7209fdf76b99d
phyc11 <xuanphuc111104@gmail.com>
2025-04-11 17:06:29 +0700

    Update project

--- spark_stream.py
+++ spark_stream.py
@@ -3,9 +3,10 @@ from pyspark.sql.functions import from_json, col, to_json, struct
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 
 spark = SparkSession.builder \
-    .appName("KafkaToElasticsearch") \
+    .appName("WeatherForscast") \
     .config("spark.jars.packages", 
-            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,org.apache.hadoop:hadoop-common:3.3.6,org.apache.hadoop:hadoop-hdfs:3.3.6") \
+    .config("spark.hadoop.fs.defaultFS", "hdfs://namenode:9000") \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("ERROR")
@@ -16,6 +17,7 @@ df = spark.readStream \
     .option("kafka.bootstrap.servers", "broker:29092") \
     .option("subscribe", "weather_forecast") \
     .option("startingOffsets", "earliest") \
+    .option("failOnDataLoss", "false") \
     .load()
 
 # Định nghĩa schema
@@ -39,7 +41,18 @@ df_selected = df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), data_schema).alias("data")) \
     .select("data.*")
 
-# # Thêm một trường id cho Elasticsearch (tùy chọn, nhưng hữu ích)
+
+query = df_selected.writeStream \
+    .format("csv") \
+    .option("path", "hdfs://namenode:9000/tmp/weather_data") \
+    .option("checkpointLocation", "hdfs://namenode:9000/tmp/checkpoints") \
+    .option("header", "true") \
+    .outputMode("append") \
+    .start()
+
+query.awaitTermination()
+
+# Thêm một trường id cho Elasticsearch (tùy chọn, nhưng hữu ích)
 # df_with_id = df_selected.withColumn("id", 
 #                                     col("location") + "_" + col("time").cast("string"))
 
@@ -57,15 +70,15 @@ df_selected = df.selectExpr("CAST(value AS STRING)") \
 #     .option("es.nodes.wan.only", "true") \
 #     .start()
 
-query = df_selected.writeStream \
-    .format("csv") \
-    .option("path", "/opt/bitnami/spark/output") \
-    .option("checkpointLocation", "/tmp/checkpoint") \
-    .option("header", "true") \
-    .outputMode("append") \
-    .start()
+# query = df_selected.writeStream \
+#     .format("csv") \
+#     .option("path", "/opt/bitnami/spark/output") \
+#     .option("checkpointLocation", "/tmp/checkpoint") \
+#     .option("header", "true") \
+#     .outputMode("append") \
+#     .start()
 
-query.awaitTermination()
+# query.awaitTermination()
 
 # console_query = df_selected.writeStream \
 #     .outputMode("append") \
